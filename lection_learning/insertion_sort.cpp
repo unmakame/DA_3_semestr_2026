757a5a98b573a4552d37a41b1cb50fed965470de
unmakame <bayanovlad@gmail.com>
2026-02-20 14:32:52 +0300

    radix_sort addet

--- lection_learning/insertion_sort.cpp
+++ lection_learning/insertion_sort.cpp
@@ -4,11 +4,11 @@
 
 
 void insertion_sort(std::vector<int> &v){
-    for(int i = 1;i < v.size();i++){
+    for(int i = 0; i < v.size(); i++){
         int key = v[i];
         int j = i - 1;
 
-        while(j >= 0 && v[j] > key){
+        while (j >= 0 && v[j] > key){
             v[j + 1] = v[j];
             j--;
         }
